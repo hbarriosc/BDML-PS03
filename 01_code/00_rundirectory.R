1f74baa6d93b754178f6a04c8d651ba27fa32b48
hbarriosc <h.barriosc@uniandes.edu.co>
2026-04-24 17:10:51 -0500

    Crear carpeta data

--- 01_code/00_rundirectory.R
+++ 01_code/00_rundirectory.R
@@ -18,6 +18,10 @@
 cat("\014")
 rm(list = ls())
 
+for (path in c("02_data")) {
+  dir.create(path, recursive = TRUE, showWarnings = FALSE)
+}
+
 for (path in c("03_output", "03_output/figures", "03_output/tables", "03_output/submissions")) {
   dir.create(path, recursive = TRUE, showWarnings = FALSE)
 }
